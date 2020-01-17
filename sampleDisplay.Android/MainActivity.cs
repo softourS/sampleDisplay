f8c0c2c884c72c3298dfb1be8faf0545706fadd4
softourS <manuel@softoursistemas.com>
2020-01-17 10:58:25 +0100

    First commit

---
+++ sampleDisplay.Android/MainActivity.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using Android;
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V4.App;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+namespace sampleDisplay.Droid {
+    //Theme = "@android:style/Theme.Holo.Light.DarkActionBar",
+    //Theme = "@style/MainTheme",
+    [Activity (Label = "sampleDisplay", Icon = "@mipmap/icon", Theme = "@android:style/Theme.Holo.Light.DarkActionBar",
+        MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, ScreenOrientation = ScreenOrientation.Portrait)]
+    public class MainActivity : FormsApplicationActivity {
+        protected override void OnCreate (Bundle savedInstanceState) {
+            //TabLayoutResource = Resource.Layout.Tabbar;
+            //ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate (savedInstanceState);
+            Xamarin.Forms.Forms.Init (this, savedInstanceState);
+            Xamarin.FormsMaps.Init (this, savedInstanceState);
+            Xamarin.Essentials.Platform.Init (this, savedInstanceState);
+
+            ActivityCompat.RequestPermissions (this, new string[] { Manifest.Permission.AccessFineLocation }, 0);
+            ActivityCompat.RequestPermissions (this, new string[] { Manifest.Permission.AccessCoarseLocation }, 0);
+            LoadApplication (new App ());
+        }
+    }
+}
---
+++ sampleDisplay.Android/obj/Debug/android/typemap.jm.x86_64.s
@@ -0,0 +1,25 @@
+	/* Data Hash: A9B3B7673B2F2213 */
+	.file	"typemap.jm.inc"
+
+	/* Mapping header */
+	.section	.data.jm_typemap,"aw",@progbits
+	.type	jm_typemap_header, @object
+	.p2align	2
+	.global	jm_typemap_header
+jm_typemap_header:
+	/* version */
+	.long	1
+	/* entry-count */
+	.long	1722
+	/* entry-length */
+	.long	262
+	/* value-offset */
+	.long	117
+	.size	jm_typemap_header, 16
+
+	/* Mapping data */
+	.type	jm_typemap, @object
+	.global	jm_typemap
+jm_typemap:
+	.size	jm_typemap, 451165
+	.include	"typemap.jm.inc"
